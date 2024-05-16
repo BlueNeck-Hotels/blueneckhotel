55b48843af232d45af5a037f5b620b08299e75af
pankajmehra <mehrapankaj383@gamil.com>
2024-05-16 18:53:38 +0530

    second command

--- assest/js/slick.js
+++ assest/js/slick.js
@@ -1,37 +1,5 @@
 
-$(document).ready(function () {
-  $('.product_slider , .categorey_slider').slick({
 
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-});
 
 
 $(document).ready(function () {
@@ -113,7 +81,7 @@ $(document).ready(function () {
   
 $('.company_slider').slick({
     infinite: true,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1,
