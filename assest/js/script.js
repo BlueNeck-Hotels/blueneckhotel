e133624dd5a25bef67c151fc662253581b84ff12
pankajmehra <pankaj@blueneckhotel.com>
2024-05-19 15:02:43 +0530

    friday second  commt

--- assest/js/script.js
+++ assest/js/script.js
@@ -25,7 +25,7 @@
 
 document.querySelector(".menu-bar").addEventListener("click", () => {
   console.log("hello")
-    document.querySelector(".main-mobile-bg").classList.toggle('toggle');
+    document.querySelector(".color").classList.toggle('toggle');
 });
 
 document.querySelector(".crose").addEventListener("click", () => {
@@ -34,6 +34,96 @@ document.querySelector(".crose").addEventListener("click", () => {
 
 
 
+// ========================date=picker==========================
+
+let nowTemp = new Date();
+let now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+let checkin = $('#dp1').datepicker({
+
+  beforeShowDay: function(date) {
+    return date.valueOf() >= now.valueOf();
+  },
+  autoclose: true
+
+}).on('changeDate', function(ev) {
+  if (ev.date.valueOf() > checkout.datepicker("getDate").valueOf() || !checkout.datepicker("getDate").valueOf()) {
+
+    let newDate = new Date(ev.date);
+    newDate.setDate(newDate.getDate() + 1);
+    checkout.datepicker("update", newDate);
+
+  }
+  $('#dp2')[0].focus();
+});
+
+
+let checkout = $('#dp2').datepicker({
+  beforeShowDay: function(date) {
+    if (!checkin.datepicker("getDate").valueOf()) {
+      return date.valueOf() >= new Date().valueOf();
+    } else {
+      return date.valueOf() > checkin.datepicker("getDate").valueOf();
+    }
+  },
+  autoclose: true
+
+}).on('changeDate', function(ev) {});
 
 
 
+
+
+// =============form=value==========================
+
+// const form=document.getElementById("form");
+// const where= document.getElementById("where");
+// const checkin=document.getElementById("dp1");
+// const checkout=document.getElementById("dp2");
+// const guest=document.getElementById("add-guest");
+
+
+// form.addEventListener('submit',function(e){
+//   e.preventDefault();
+//  const whereval= where.value;
+//  const checkinval= checkin.value;
+//  const checkoutval= checkout.value;
+//  const guestval= guest.value;
+
+//  localStorage.setItem('where-val' ,whereval);
+//  localStorage.setItem('checkin-val' ,checkinval);
+//  localStorage.setItem('checkout-val' ,checkoutval);
+//   localStorage.setItem('guest-val' ,guestval);
+
+//   window.location.href="hotel-listing.html"
+// })
+
+document.getElementById("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  
+  localStorage.setItem("where-val", document.getElementById("where").value);
+  localStorage.setItem("checkin-val", document.getElementById("dp1").value);
+  localStorage.setItem("checkout-val", document.getElementById("dp2").value);
+  localStorage.setItem("guest-val", document.getElementById("add-guest").value);
+  
+  window.location.href = "hotel-listing.html";
+});
+
+
+// ======================readmore=================================
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
